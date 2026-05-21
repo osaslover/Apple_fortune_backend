4acd844a8234a5cfd1c31c828ef63feb64aefd65
osaslover <osaslover31542@gmail.com>
2026-05-22 00:44:46 +0100

    Update env.js

--- Config/env.js
+++ Config/env.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+
+  NODE_ENV:
+    process.env.NODE_ENV || 'development',
+
+  DATABASE_URL:
+    process.env.DATABASE_URL || '',
+
   JWT_SECRET:
     process.env.JWT_SECRET ||
     'default_jwt_secret_123456789',
